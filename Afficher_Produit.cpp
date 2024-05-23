40223ca2fa694d724b14a70b5d334028b1de66cc
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-23 20:01:57 +0100

    Afficher les Produits

---
+++ Afficher_Produit.cpp
@@ -0,0 +1,28 @@
+void Afficher_Produit(const vector<Produit>& Produits) {
+    char choix;
+
+    cout << "=============================================" << endl;
+    cout << "               Produits disponibles" << endl;
+    cout << "=============================================" << endl;
+    for (const auto& article : Produits) {
+        article.afficher_article();
+        cout << endl;
+    }
+
+    do {
+        cout << "Appuyez sur 'm' pour revenir au menu principal ou 'q' pour quitter : ";
+        cin >> choix;
+
+        if (choix == 'm') {
+            effacer_ecran();
+            return;
+        }
+        else if (choix == 'q') {
+            effacer_ecran();
+            cout << "=============================================" << endl;
+            cout << "                  Au revoir !" << endl;
+            cout << "=============================================" << endl;
+            exit(0);
+        }
+    } while (true);
+}
