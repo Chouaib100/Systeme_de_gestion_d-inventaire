0dd28436636db840ce5ed944659423460d5222ae
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:07:48 +0100

    include/ déclaration Afficher les informatuins d'un Produit

---
+++ Afficher_infos_Produit.h
@@ -0,0 +1,11 @@
+#ifndef AFFICHER_INFOS_PRODUIT_H
+#define AFFICHER_INFOS_PRODUIT_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Afficher_informations_Produit(const vector<Produit>& Produits);
+
+#endif
