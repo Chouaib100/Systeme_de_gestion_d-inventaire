3d530c97a6dfc933f231c239c0e4c67686a1d7a1
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:28:42 +0100

    Modifier un Produit

--- Modifier_Produit.cpp
+++ Modifier_Produit.cpp
@@ -1,14 +1,20 @@
-void Modifier_Produit(vector<Produit>& Produits) {
-    effacer_ecran();
-    cout << "=============================================" << endl;
-    cout << "             Modifier un Produit !" << endl;
-    cout << "=============================================" << endl;
+#include "Modifier_Produit.h"
+#include <iostream>
+#include "effacer_ecran.h"
 
+using namespace std;
+
+void Modifier_Produit(vector<Produit>& Produits) {
     int ref;
     int choixAttribut;
     int nouvel_attribut;
     char nouvelle_designation[100];
     char nouvelle_zone[100];
+
+    effacer_ecran();
+    cout << "=============================================" << endl;
+    cout << "             Modifier un Produit !" << endl;
+    cout << "=============================================" << endl;
 
     cout << "Entrez la référence de Produit à modifier : ";
     cin >> ref;
@@ -31,36 +37,45 @@ void Modifier_Produit(vector<Produit>& Produits) {
             cin >> choixAttribut;
 
             switch (choixAttribut) {
-
-
-            default:
-                cout << "Choix invalide." << endl;
-                char choix;
-                do {
-                    cout << "Appuyez sur 'm' pour revenir au menu principal, 'q' pour quitter, ou 'r' pour modifier un autre Produits : ";
-                    cin >> choix;
-
-                    if (choix == 'm') {
-                        effacer_ecran();
-                        return;
-                    }
-                    else if (choix == 'q') {
-                        effacer_ecran();
-                        cout << "=============================================" << endl;
-                        cout << "                  Au revoir !" << endl;
-                        cout << "=============================================" << endl;
-                        exit(0);
-                    }
-                    else if (choix == 'r') {
-                        effacer_ecran();
-                        Modifier_Produit(Produits);
-                    }
-                    else {
-                        cout << "Choix invalide. Veuillez entrer 'm', 'q' ou 'r' : ";
-                    }
-                } while (true);
+                case 1:
+                    cout << "Entrez la nouvelle désignation du produit : ";
+                    cin.ignore();
+                    cin.getline(nouvelle_designation, 100);
+                    article.setdesignation(nouvelle_designation);
+                    break;
+                case 2:
+                    cout << "Entrez le nouveau prix du produit : ";
+                    cin >> nouvel_attribut;
+                    article.setprix(nouvel_attribut);
+                    break;
+                case 3:
+                    cout << "Entrez la nouvelle quantité disponible : ";
+                    cin >> nouvel_attribut;
+                    article.setquantiteDisponible(nouvel_attribut);
+                    break;
+                case 4:
+                    cout << "Entrez la nouvelle zone de l'emplacement : ";
+                    cin.ignore();
+                    cin.getline(nouvelle_zone, 100);
+                    article.set_emplacement_zone(nouvelle_zone);
+                    break;
+                case 5:
+                    cout << "Entrez la nouvelle rangée de l'emplacement : ";
+                    cin >> nouvel_attribut;
+                    article.set_emplacement_rangee(nouvel_attribut);
+                    break;
+                case 6:
+                    cout << "Entrez la nouvelle étagère de l'emplacement : ";
+                    cin >> nouvel_attribut;
+                    article.set_emplacement_etagere(nouvel_attribut);
+                    break;
+                default:
+                    cout << "Choix invalide." << endl;
+                    break;
             }
+
             effacer_ecran();
+            cout << "Produit modifié avec succès." << endl;
             return;
         }
     }
