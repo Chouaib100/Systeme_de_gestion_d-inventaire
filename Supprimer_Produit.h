f220fb128c2b2c8984cf8901e3565d15fb3441f4
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:10:34 +0100

    include/  déclaration Supprimer un Produit

---
+++ Supprimer_Produit.h
@@ -0,0 +1,11 @@
+#ifndef SUPPRIMER_PRODUIT_H
+#define SUPPRIMER_PRODUIT_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Supprimer_Produit(vector<Produit>& Produits);
+
+#endif
