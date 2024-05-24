05b0dd4eba3108c83326f55fc9fd0ca4ed8c5f92
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:06:03 +0100

    include/ déclaration Afficher un Produit

---
+++ Afficher_Produit.h
@@ -0,0 +1,11 @@
+#ifndef AFFICHER_PRODUIT_H
+#define AFFICHER_PRODUIT_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Afficher_Produit(const vector<Produit>& Produits);
+
+#endif
