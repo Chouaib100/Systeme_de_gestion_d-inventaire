2302b2ac7f954ebb3e272f393227cac4f627897e
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:00:46 +0100

    Classe Produit

--- Produit.cpp
+++ Produit.cpp
@@ -1,63 +1,43 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include "Produit.h"
+
 using namespace std;
 
+Produit::Produit(int ref, const char* d, int p, const char* z, int r, int qu, int e)
+    : emplacement(z, r, e) {
+    reference = ref;
+    designation = new char[strlen(d) + 1];
+    strcpy(designation, d);
+    prix = p;
+    quantiteDisponible = qu;
+}
 
-class Produit {
-private:
-    int reference;
-    char* designation;
-    int prix;
-    int quantiteDisponible;
-    Emplacement emplacement;
+void Produit::afficher_article() const {
+    cout << "  Reference: " << reference << endl;
+    cout << "  Designation: " << designation << endl;
+    cout << "  Prix: " << prix << " dirhams" << endl;
+    cout << "  Quantite disponible: " << quantiteDisponible << endl;
+    cout << "  Emplacement: \n";
+    emplacement.afficher_emplacement();
+}
 
-public:
-    Produit(int ref = -1, const char* d = NULL, int p = -1, const char* z = NULL, int r = -1, int qu = -1, int e = -1)
-        : emplacement(z, r, e) {
-        reference = ref;
-        designation = new char[strlen(d) + 1];
-        strcpy(designation, d);
-        prix = p;
-        quantiteDisponible = qu;
-    }
+int Produit::getreference() const { return reference; }
+char* Produit::getrdesignation() const { return designation; }
+int Produit::getprix() const { return prix; }
+int Produit::getquantiteDisponible() const { return quantiteDisponible; }
+char* Produit::get_emplacement_zone() const { return emplacement.getzone(); }
+int Produit::get_emplacement_rangee() const { return emplacement.getrangee(); }
+int Produit::get_emplacement_etagere() const { return emplacement.getetagere(); }
 
-    void afficher_article() const {
-        cout << "  Reference: " << reference << endl;
-        cout << "  Designation: " << designation << endl;
-        cout << "  Prix: " << prix << " dirhams" << endl;
-        cout << "  Quantite disponible: " << quantiteDisponible << endl;
-        cout << "  Emplacement: \n";
-        emplacement.afficher_emplacement();
-    }
+void Produit::setreference(int ref) { reference = ref; }
 
-    int getreference() const { return reference; }
-    char* getrdesignation() const { return designation; }
-    int getprix() const { return prix; }
-    int getquantiteDisponible() const { return quantiteDisponible; }
-    char* get_emplacement_zone() const { return emplacement.getzone(); }
-    int get_emplacement_rangee() const { return emplacement.getrangee(); }
-    int get_emplacement_etagere() const { return emplacement.getetagere(); }
+void Produit::setdesignation(const char* d) {
+    delete[] designation;
+    designation = new char[strlen(d) + 1];
+    strcpy(designation, d);
+}
 
-    void setreference(int ref) { reference = ref; }
-
-    void setdesignation(char* d) {
-        if (d == NULL) {
-            delete[] designation;
-            designation = new char[strlen(d) + 1];
-            strcpy(designation, d);
-        }
-        else {
-            designation = new char[strlen(d) + 1];
-            strcpy(designation, d);
-        }
-    }
-
-    void setprix(int p) { prix = p; }
-
-    void setquantiteDisponible(int qu) { quantiteDisponible = qu; }
-
-    void set_emplacement_zone(char* z) { emplacement.setzone(z); }
-    void set_emplacement_rangee(int r) { emplacement.setrangee(r); }
-    void set_emplacement_etagere(int e) { emplacement.setetagere(e); }
-};
+void Produit::setprix(int p) { prix = p; }
+void Produit::setquantiteDisponible(int qu) { quantiteDisponible = qu; }
+void Produit::set_emplacement_zone(const char* z) { emplacement.setzone(z); }
+void Produit::set_emplacement_rangee(int r) { emplacement.setrangee(r); }
+void Produit::set_emplacement_etagere(int e) { emplacement.setetagere(e); }
