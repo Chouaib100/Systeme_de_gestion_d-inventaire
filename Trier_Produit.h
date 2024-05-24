13a71a9eae470eb630f38b5f2c9fd11b2eb3555c
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:14:18 +0100

    include/ déclaration Trier UN Produit

---
+++ Trier_Produit.h
@@ -0,0 +1,11 @@
+#ifndef TRIER_PRODUIT_H
+#define TRIER_PRODUIT_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Trier_Produits_par_prix_decroissant(vector<Produit>& Produits);
+
+#endif
