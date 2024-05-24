72046034870a5c9bf1b746434dcea140f27d8657
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:35:51 +0100

    Executer le menu prancipale

--- Executer_menu.cpp
+++ Executer_menu.cpp
@@ -1,41 +1,42 @@
+#include "executer_menu.h"
+#include <iostream>
+
+using namespace std;
+
 void executer_menu(vector<Produit>& Produits) {
     int choix;
     do {
         choix = Afficher_menu();
         switch (choix) {
-        case 1:
-            Creer_Produit(Produits);
-            break;
-        case 2:
-            Afficher_informations_Produit(Produits);
-            break;
-        case 3:
-            effacer_ecran();
-            Modifier_Produit(Produits);
-            break;
-        case 4:
-            effacer_ecran();
-            Afficher_Produit(Produits);
-            break;
-        case 5:
-            effacer_ecran();
-            Supprimer_Produit(Produits);
-            break;
-        case 6:
-            Trier_Produits_par_prix_decroissant(Produits);
-            break;
-        case 7:
-            effacer_ecran();
-            Afficher_Produit_quantite_nulle(Produits);
-            break;
-        case 8:
-            effacer_ecran();
-            cout << "=============================================" << endl;
-            cout << "                  Au revoir !" << endl;
-            cout << "=============================================" << endl;
-            break;
-        default:
-            cout << "Choix invalide. Veuillez entrer un nombre entre 1 et 7." << endl;
+            case 1:
+                Creer_Produit(Produits);
+                break;
+            case 2:
+                Afficher_informations_Produit(Produits);
+                break;
+            case 3:
+                Modifier_Produit(Produits);
+                break;
+            case 4:
+                Afficher_Produit(Produits);
+                break;
+            case 5:
+                Supprimer_Produit(Produits);
+                break;
+            case 6:
+                Trier_Produits_par_prix_decroissant(Produits);
+                break;
+            case 7:
+                Afficher_Produit_quantite_nulle(Produits);
+                break;
+            case 8:
+                effacer_ecran();
+                cout << "=============================================" << endl;
+                cout << "                  Au revoir !" << endl;
+                cout << "=============================================" << endl;
+                break;
+            default:
+                cout << "Choix invalide. Veuillez entrer un nombre entre 1 et 7." << endl;
         }
     } while (choix != 8);
 }
