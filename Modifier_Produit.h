61f4f959b8e255c35e545de0de9ffbd7a42f3d4c
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:09:09 +0100

    include/ déclaration Modifier un Produit

---
+++ Modifier_Produit.h
@@ -0,0 +1,11 @@
+#ifndef MODIFIER_PRODUIT_H
+#define MODIFIER_PRODUIT_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Modifier_Produit(vector<Produit>& Produits);
+
+#endif
