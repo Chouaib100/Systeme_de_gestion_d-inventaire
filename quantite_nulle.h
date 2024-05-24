65130d45aaaa78bb563c11879a479cf157e91536
Chouaib Zouine <chouaibzouine@gmail.com>
2024-05-25 00:16:51 +0100

    include/ déclaration ProduitS de quantite nulle

---
+++ quantite_nulle.h
@@ -0,0 +1,11 @@
+#ifndef QUANTITE_NULLE_H
+#define QUANTITE_NULLE_H
+
+#include <vector>
+#include "Produit.h"
+
+using namespace std;
+
+void Afficher_Produit_quantite_nulle(const vector<Produit>& Produits);
+
+#endif
